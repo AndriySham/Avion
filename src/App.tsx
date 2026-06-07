f3cdaafe189579f9bf3fcffacccc7d5c24dbd5dc
AndriySham <avshamatrin@gmail.com>
2026-06-07 11:09:15 +0300

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#2A254B' }}>Avion Furniture Store</h1>
+      <p style={{ color: '#726E8D' }}>Ласкаво просимо! Проєкт успішно очищено та готовий до роботи.</p>
+    </div>
+  )
+}
+
+export default App
