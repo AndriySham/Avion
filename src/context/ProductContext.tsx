9fd807dc8bf96ac1b69b2547597c0d382d131764
AndriySham <avshamatrin@gmail.com>
2026-06-17 20:24:44 +0300

    Add ProductsContext

---
+++ src/context/ProductContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useState, useEffect, type ReactNode } from "react";
+import type { Product } from "../types";
+import { getProducts } from "../services/api";
+
+export interface ProductsContextType {
+    products: Product[] | null;
+    isLoading: boolean;
+}
+
+const ProductsContext = createContext<ProductsContextType | undefined>(undefined);
+
+export function ProductsProvider({ children }: { children: ReactNode }) {
+    const [products, setProducts] = useState<Product[] | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+
+        const loadProducts = async () => {
+            const result = await getProducts();
+
+            if (result === null) {
+                setError("Failed to load products");
+                setIsLoading(false);
+                return;
+            }
+
+            setProducts(result);
+            setIsLoading(false);
+        };
+
+        loadProducts();
+    }, []);
+
+    return (
+        <ProductsContext value={{ products, isLoading }}>
+            {children}
+        </ProductsContext>
+    );
+}
+
+export function useProducts() {
+    const context = useContext(ProductsContext);
+
+    if (context === undefined) {
+        throw new Error('useProducts must be used within ProductsProvider');
+    }
+
+    return context;
+}
